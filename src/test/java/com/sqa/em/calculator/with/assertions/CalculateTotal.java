f02576acbd8181469a942a0e8cf3abb93c0b318c
100101Ed <edm100101@hotmail.com>
2016-02-13 09:19:55 -0800

    Add Calculate Total

--- src/test/java/com/sqa/em/calculator/with/assertions/CalculateTotal.java
+++ src/test/java/com/sqa/em/calculator/with/assertions/CalculateTotal.java
@@ -16,19 +16,17 @@ import org.junit.runners.Parameterized.Parameters;
 import com.sqa.em.util.helper.IdentifyObject;
 
 /**
- * v
- *
  * @author Mora, Eduardo
  * @version 1.0.0
  * @since 1.0
  */
 @RunWith(Parameterized.class)
-public class CalculateMultiplication {
+public class CalculateTotal {
 
 	@Parameters
 	public static Object[][] getData() {
-		Object[][] data = { { "Test 1", 5, 6, 30 }, { "Test 2", 50, 16, 800 }, { "Test 3", 35, 46, 1610 },
-				{ "Test 4", 4, 6, 24 }, { "Test 5", 45, 5, 225 }, { "Test 6", 5, 6, 100 } };
+		Object[][] data = { { "Test 1", 12, 6, 18 }, { "Test 2", 14, 2, 16 }, { "Test 3", 100, 1, 101 },
+				{ "Test 4", 4, 6, 10 }, { "Test 5", 45, 5, 50 }, { "Test 6", 5, 6, 100 } };
 		return data;
 	}
 
@@ -40,22 +38,20 @@ public class CalculateMultiplication {
 
 	int expected;
 
-	public CalculateMultiplication(String label, int num1, int num2, int expected) {
+	public CalculateTotal(String label, int num1, int num2, int expected) {
 		this.label = label;
 		this.num1 = num1;
 		this.num2 = num2;
 		this.expected = expected;
 	}
 
-	/**
-	 *
-	 */
 	@Test
-	public void calculteMultiplicationTest() {
+	public void divideNumbersTest() {
 		System.out.println(this.label);
 		IdentifyObject.display2DInfo(getData());
-		int total = this.num1 * this.num2;
+		int total = this.num1 + this.num2;
+		System.out.print("Tota = " + total);
 		Assert.assertEquals(this.expected, total);
-		System.out.println("Passed " + total);
+		System.out.println(" Passed ");
 	}
 }
